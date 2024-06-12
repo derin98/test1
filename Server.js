efbb8d3cbf038d3bd4a63f1d182ddaaaeedd47f1
derin_crion <derin@criontech.com>
2024-06-12 15:04:05 +0530

    init

---
+++ Server.js
@@ -0,0 +1,16 @@
+/**
+ * This file will be the start point of the application.
+ */
+const serverConfig = { PORT: 3000 };
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Application started on the port num : ${serverConfig.PORT}`);
+});
